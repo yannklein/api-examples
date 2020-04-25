519b9095c355504969c7c9720836f1d5daa78f24
Yann Klein <yann.klein@me.com>
2020-04-25 17:58:47 +0900

    index.html refactoring

--- geocoding/index.js
+++ geocoding/index.js
@@ -1,39 +1,40 @@
-const findThis = (address) => {
+// const findThis = (address) => {
 
-  // Translate the address into coordinates
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoieWFubmx1Y2tsZWluIiwiYSI6ImNqcnZmeHQwaDAxb2o0NGx2bG1tOWgwNGIifQ.q4zhKOCoH7nDIJNm88leXg`;
-  fetch(url)
-  .then(response => response.json())
-  .then((data) => {
+//   // Translate the address into coordinates
+//   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoieWFubmx1Y2tsZWluIiwiYSI6ImNqcnZmeHQwaDAxb2o0NGx2bG1tOWgwNGIifQ.q4zhKOCoH7nDIJNm88leXg`;
+//   fetch(url)
+//   .then(response => response.json())
+//   .then((data) => {
 
-    const coordinates = data.features[0].center // this are the new coordinates (e.g.[139.839478, 35.652832])
+//     const coordinates = data.features[0].center; // this are the new coordinates (e.g.[139.839478, 35.652832])
+//     fetchMessage.innerText = coordinates;
 
-    // Move the map to the new coordinate point
-    map.setCenter(coordinates);
-    var marker = new mapboxgl.Marker()
-      .setLngLat(coordinates)
-      .addTo(map);
-    })
+//     // // Move the map to the new coordinate point
+//     // map.setCenter(coordinates);
+//     // var marker = new mapboxgl.Marker()
+//     //   .setLngLat(coordinates)
+//     //   .addTo(map);
+//   })
 
-};
-
+// };
 
 // ///////////////// Execute the code /////////////////
 
-// Initialise the Map, already an API call to Mapbox!
-mapboxgl.accessToken = 'pk.eyJ1IjoieWFubmx1Y2tsZWluIiwiYSI6ImNqcnZmeHQwaDAxb2o0NGx2bG1tOWgwNGIifQ.q4zhKOCoH7nDIJNm88leXg';
-const map = new mapboxgl.Map({
-  container: 'map', // container id
-  style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-  center: [139.839478, 35.652832], // starting on Tokyo
-  zoom: 9 // starting zoom
-});
+// // Initialise the Map, already an API call to Mapbox!
+// mapboxgl.accessToken = 'pk.eyJ1IjoieWFubmx1Y2tsZWluIiwiYSI6ImNqcnZmeHQwaDAxb2o0NGx2bG1tOWgwNGIifQ.q4zhKOCoH7nDIJNm88leXg';
+// const map = new mapboxgl.Map({
+//   container: 'map', // container id
+//   style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+//   center: [139.839478, 35.652832], // starting on Tokyo
+//   zoom: 9 // starting zoom
+// });
 
-// Listen to a new address
-const input = document.querySelector("input");
-const button = document.querySelector("button");
-button.addEventListener("click", (event) => {
-  // Relocate our map to the new address
-  findThis(input.value);
-});
+// // Listen to a new address
+// const input = document.querySelector("input");
+// const button = document.querySelector("button");
+// const fetchMessage = document.querySelector(".message");
+// button.addEventListener("click", (event) => {
+//   // Relocate our map to the new address
+//   findThis(input.value);
+// });
 
--- open-banking/index.js
+++ open-banking/index.js
@@ -1,71 +1,74 @@
-const chooseBank = (chosenBank) => {
-  const url = `https://raw.githubusercontent.com/OpenBankingUK/opendata-api-spec-compiled/master/participant_store.json`;
-  fetch(url)
-  .then(response => response.json())
-  .then((data) => {
-      data.data.forEach((bank) => {
-        if (bank.name.toLowerCase().includes(chosenBank.toLowerCase())) {
-          findATMs(bank.name, `${bank.baseUrl}/${bank.supportedAPIs.atms}/atms`);
-        }
-      });
-  });
-}
+// const chooseBank = (chosenBank) => {
+//   const url = `https://raw.githubusercontent.com/OpenBankingUK/opendata-api-spec-compiled/master/participant_store.json`;
+//   fetch(url)
+//   .then(response => response.json())
+//   .then((data) => {
+//       data.data.forEach((bank) => {
+//         if (bank.name.toLowerCase().includes(chosenBank.toLowerCase())) {
+//           fetchMessage.insertAdjacentHTML("beforeend", `<p>We found ${bank.name} on Open Banking UK!</p>`);
+//           // findATMs(bank.name, `${bank.baseUrl}/${bank.supportedAPIs.atms}/atms`);
+//         }
+//       });
+//   });
+// }
 
-const findATMs = (bankName, url) => {
-  // Translate the address into coordinates
-  console.log(url);
-  fetch(url, {headers: {'Accept': 'application/prs.openbanking.opendata.v2.2+json'}})
-  .then(response => response.json())
-  .then((data) => {
+// const findATMs = (bankName, url) => {
+//   // Translate the address into coordinates
+//   console.log(url);
+//   fetch(url, {headers: {'Accept': 'application/prs.openbanking.opendata.v2.2+json'}})
+//   .then(response => response.json())
+//   .then((data) => {
 
-    errorMessage.insertAdjacentHTML("beforeend", `<p>We found some ATMs for ${bankName}!</p>`);
-    const atms = data.data[0].Brand[0].ATM // this are the new coordinates (e.g.[139.839478, 35.652832])
+//     fetchMessage.insertAdjacentHTML("beforeend", `<p>We found some ATMs for ${bankName}!</p>`);
+//     const atms = data.data[0].Brand[0].ATM // this are the new coordinates (e.g.[139.839478, 35.652832])
 
-    atms.forEach((atm) => {
-      const coordinates = atm.Location.PostalAddress.GeoLocation.GeographicCoordinates;
-      // console.log(coordinates);
-      // Move the map to the new coordinate point
-      markerArray.push(
-        new mapboxgl.Marker()
-        .setLngLat([coordinates.Longitude, coordinates.Latitude])
-        .addTo(map)
-      );
-    })
-  })
-  .catch((error) => {
-    errorMessage.insertAdjacentHTML("beforeend", `<p>No luck for ${bankName}! You'll need to signup to use the API...</p>`);
-  });
-};
+//     atms.forEach((atm) => {
+//       const coordinates = atm.Location.PostalAddress.GeoLocation.GeographicCoordinates;
+//       // console.log(coordinates);
+//       // Move the map to the new coordinate point
+//       markerArray.push(
+//         new mapboxgl.Marker()
+//         .setLngLat([coordinates.Longitude, coordinates.Latitude])
+//         .addTo(map)
+//       );
+//     })
+//   })
+//   .catch((error) => {
+//     fetchMessage.insertAdjacentHTML("beforeend", `<p>No luck for ${bankName}! You'll need to signup to use the API...</p>`);
+//   });
+// };
+
+
+
 
 
 // ///////////////// Execute the code /////////////////
 
-const markerArray = [];
+// const markerArray = [];
 
-// Initialise the Map, already an API call to Mapbox!
-mapboxgl.accessToken = 'pk.eyJ1IjoieWFubmx1Y2tsZWluIiwiYSI6ImNqcnZmeHQwaDAxb2o0NGx2bG1tOWgwNGIifQ.q4zhKOCoH7nDIJNm88leXg';
-const map = new mapboxgl.Map({
-  container: 'map', // container id
-  style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-  center: [-3.177109, 54.029150], // starting on Tokyo
-  zoom: 5 // starting zoom
-});
+// // Initialise the Map, already an API call to Mapbox!
+// mapboxgl.accessToken = 'pk.eyJ1IjoieWFubmx1Y2tsZWluIiwiYSI6ImNqcnZmeHQwaDAxb2o0NGx2bG1tOWgwNGIifQ.q4zhKOCoH7nDIJNm88leXg';
+// const map = new mapboxgl.Map({
+//   container: 'map', // container id
+//   style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+//   center: [-3.177109, 54.029150], // starting on Tokyo
+//   zoom: 5 // starting zoom
+// });
 
-// Listen to a new address
-const input = document.querySelector(".input");
-const button = document.querySelector(".button");
-const errorMessage = document.querySelector(".error-message");
-button.addEventListener("click", (event) => {
 
-  // Remove the previous markers
-  markerArray.forEach((marker) => marker.remove());
-  errorMessage.innerText = "";
 
-  // Find the Lloyds ATMS
-  // const url = `https://api.lloydsbank.com/open-banking/v2.2/atms`;
-  // findATMs();
 
-  // Find the ATMs of the designated bank
-  chooseBank(input.value);
-});
+// // Listen to a new address
+// const input = document.querySelector(".input");
+// const button = document.querySelector(".button");
+// const fetchMessage = document.querySelector(".message");
+// button.addEventListener("click", (event) => {
+
+//   // Remove the previous markers
+//   markerArray.forEach((marker) => marker.remove());
+//   fetchMessage.innerText = "";
+
+//   // Find the ATMs of the designated bank
+//   chooseBank(input.value);
+// });
 
